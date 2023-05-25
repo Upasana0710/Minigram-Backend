d622b7e02cfa7a6cb5595b553fcc3443ca6902b6
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-05-25 15:21:17 +0530

    package changes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
+import compression from 'compression'
+import helmet from 'helmet'
 import userRoutes from './src/routes/user.js';
 import postRoutes from './src/routes/post.js';
 import commentRoutes from './src/routes/comments.js';
 import conversationRoutes from './src/routes/conversation.js'
-import messageRoutes from './src/routes/messages.js'
+import messageRoutes from './src/routes/messages.js';
 dotenv.config();
 
 const app = express();
@@ -17,6 +20,9 @@ mongoose.connect(process.env.CONNECTION_URL)
     }))
     .catch((err)=>console.log(err))
 
+app.use(compression());
+app.use(helmet());
+app.use(cors());
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
