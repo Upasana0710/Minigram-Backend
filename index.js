938f3bdc46c32f66f9df7c96575250267a56f914
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-05-25 15:45:09 +0530

    added cors origin

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ mongoose.connect(process.env.CONNECTION_URL)
 
 app.use(compression());
 app.use(helmet());
-app.use(cors());
+app.use(
+    cors({
+    origin: "http://localhost:3000",
+}));
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
