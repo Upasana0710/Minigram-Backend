0e9da381e1a96c35d2dc92f4ec9f0eb235cedf0e
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-01 12:43:36 +0530

    added socket.io

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import compression from 'compression'
+import { createServer } from "http";
+import { Server } from "socket.io";
 import userRoutes from './src/routes/user.js';
 import postRoutes from './src/routes/post.js';
 import commentRoutes from './src/routes/comments.js';
@@ -11,6 +13,17 @@ import messageRoutes from './src/routes/messages.js';
 dotenv.config();
 
 const app = express();
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "https://chatzbee.netlify.app/",
+  },
+});
+
+httpServer.listen(8900, () => {
+  console.log("Socket.IO server is running on port 8900");
+});
 
 const PORT = process.env.PORT || 3300;
 mongoose.connect(process.env.CONNECTION_URL)
@@ -36,3 +49,47 @@ app.use('/post',postRoutes);
 app.use('/comments',commentRoutes);
 app.use('/conversation',conversationRoutes);
 app.use('/messages',messageRoutes);
+
+//SOCKET
+let users = [];
+
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+        users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+};
+
+
+io.on("connection", (socket) => {
+    //when ceonnect
+    console.log("a user connected.");
+
+    //take userId and socketId from user
+    socket.on("addUser", (userId) => {
+        addUser(userId, socket.id);
+        io.emit("getUsers", users);
+    });
+
+    //send and get message
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        io.to(user.socketId).emit("getMessage", {
+            senderId,
+            text,
+        });
+    });
+
+    //when disconnect
+    socket.on("disconnect", () => {
+        console.log("a user disconnected!");
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+    });
+});
