9a679417db368aefadb67ea342c73151d1ad1056
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-07 11:37:01 +0530

    added a populate

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -58,7 +58,7 @@ export const getUsers = async(req, res) => {
 export const getUser = async(req, res) => {
     const {id} = req.params;
     try{
-        const user = await User.findById(id);
+        const user = await User.findById(id).populate("posts");
         res.status(200).json(user);
     }catch(error){
         console.log(error);
